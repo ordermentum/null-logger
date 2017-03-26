5092d867d70c1e25d07b5a8cab534097a8e4bd76
John D'Agostino <john@ordermentum.com>
2017-03-27 08:16:55 +1100

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const NULL_LOGGER = {
+  info: () => {},
+  error: () => {},
+  trace: () => {},
+  warn: () => {},
+  fatal: () => {},
+  debug: () => {},
+  log: () => {},
+};
+
+module.exports = NULL_LOGGER;
